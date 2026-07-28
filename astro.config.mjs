d8b7effedbead34eb0047a9b649efbcea7c50043
carles60 <cbtinternet@gmail.com>
2026-07-28 16:56:05 +0200

    Estructura inicial de Mis apuntes sobre Tricalc

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,16 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Mis apuntes sobre Tricalc',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Modelado',
+					items: [{ autogenerate: { directory: 'modelado' } }],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+                	label: 'Cargas',
+  					items: [{ autogenerate: { directory: 'cargas' } }],
 				},
 			],
 		}),
