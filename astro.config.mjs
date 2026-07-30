6ef6c802f266b59b014502ccaa004e89ddf841ea
carles60 <cbtinternet@gmail.com>
2026-07-30 17:36:58 +0200

    Primera versión de la documentación

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Mis apuntes sobre Tricalc',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			//social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Modelado',
@@ -16,6 +16,35 @@ export default defineConfig({
 				{
                 	label: 'Cargas',
   					items: [{ autogenerate: { directory: 'cargas' } }],
+				},
+				{
+  					label: 'Construcción web',
+  					items: [
+    					{
+      						label: 'Markdown',
+      						items: [
+        						{ autogenerate: { directory: 'construccion-web/markdown' } },
+      						],
+    					},
+						    					{
+      						label: 'MDX',
+      						items: [
+        						{ autogenerate: { directory: 'construccion-web/mdx' } },
+      						],
+    					},
+    					{
+      						label: 'Desarrollo',
+      						items: [
+        						{ autogenerate: { directory: 'construccion-web/desarrollo' } },
+      						],
+   						 },
+						 {
+      						label: 'Visual Studio Code',
+      						items: [
+        						{ autogenerate: { directory: 'construccion-web/visual-studio-code' } },
+      						],
+   						 },
+ 					 ],
 				},
 			],
 		}),
