aa7324bc0869fc356bea29e558e27b638064b573
carles60 <cbtinternet@gmail.com>
2026-08-06 20:34:59 +0200

    060820262034

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,153 +11,160 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'CONFIGURACIÓN',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
 					items: [
     					{
       						label: 'Archivos',
       						items: [
-        						{ autogenerate: { directory: 'configuracion' } },
+        						{ autogenerate: { directory: 'configuracion/archivos' } },
       						],
     					},
 						{
       						label: 'Interfaz',
       						items: [
-        						{ autogenerate: { directory: 'configuracion' } },
+        						{ autogenerate: { directory: 'configuracion/interfaz' } },
       						],
     					},
     					{
       						label: 'Dibujos .dwg',
       						items: [
-        						{ autogenerate: { directory: 'configuracion' } },
+        						{ autogenerate: { directory: 'configuracion/dibujos-dwg' } },
       						],
    						 },
 					],	
 				},
 				{
 					label: 'GEOMETRÍA',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
 					items: [
     					{
       						label: 'Nudos',
       						items: [
-        						{ autogenerate: { directory: 'geometria' } },
+        						{ autogenerate: { directory: 'geometria/nudos' } },
       						],
     					},
 						{
       						label: 'Barras',
       						items: [
-        						{ autogenerate: { directory: 'geometria' } },
+        						{ autogenerate: { directory: 'geometria/barras' } },
       						],
     					},
     					{
       						label: 'Pórticos',
       						items: [
-        						{ autogenerate: { directory: 'geometria' } },
+        						{ autogenerate: { directory: 'geometria/porticos' } },
       						],
    						 },
 						 {
       						label: 'Mallas',
       						items: [
-        						{ autogenerate: { directory: 'geometria' } },
+        						{ autogenerate: { directory: 'geometria/mallas' } },
       						],
    						 },
 						 {
       						label: 'Naves',
       						items: [
-        						{ autogenerate: { directory: 'geometria' } },
+        						{ autogenerate: { directory: 'geometria/naves' } },
       						],
    						 },
 					],	
 				},
 				{
 					label: 'SECCIONES',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
 					items: [
     					{
       						label: 'de Acero',
       						items: [
-        						{ autogenerate: { directory: 'secciones' } },
+        						{ autogenerate: { directory: 'secciones/de-acero' } },
       						],
     					},
 						{
       						label: 'de Hormigón',
       						items: [
-        						{ autogenerate: { directory: 'secciones' } },
+        						{ autogenerate: { directory: 'secciones/de-hormigon' } },
       						],
     					},
     					{
       						label: 'de Aluminio',
       						items: [
-        						{ autogenerate: { directory: 'secciones' } },
+        						{ autogenerate: { directory: 'secciones/de-aluminio' } },
       						],
    						 },
 						 {
       						label: 'de Madera',
       						items: [
-        						{ autogenerate: { directory: 'secciones' } },
+        						{ autogenerate: { directory: 'secciones/de-madera' } },
       						],
    						 },
 					],	
 				},
 				{
 					label: 'COMPONENTES',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
 					items: [
     					{
       						label: 'Cimentaciones',
       						items: [
-        						{ autogenerate: { directory: 'componentes' } },
+        						{ autogenerate: { directory: 'componentes/cimentaciones' } },
       						],
     					},
 						{
       						label: 'Forjados',
       						items: [
-        						{ autogenerate: { directory: 'componentes' } },
+        						{ autogenerate: { directory: 'componentes/forjados' } },
       						],
     					},
     					{
       						label: 'Muros de sótano',
       						items: [
-        						{ autogenerate: { directory: 'componentes' } },
+        						{ autogenerate: { directory: 'componentes/muros-sotano' } },
       						],
    						 },
 						 {
       						label: 'Muros resistentes',
       						items: [
-        						{ autogenerate: { directory: 'componentes' } },
+        						{ autogenerate: { directory: 'componentes/muros-resistentes' } },
       						],
    						 },
 						 {
       						label: 'Muros pantalla',
       						items: [
-        						{ autogenerate: { directory: 'componentes' } },
+        						{ autogenerate: { directory: 'componentes/muros-pantalla' } },
       						],
    						 },
 					],	
 				},
 				{
                 	label: 'CARGAS',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
   					items: [{ autogenerate: { directory: 'cargas' } }],
 				},
 				{
                 	label: 'CÁLCULO',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
   					items: [{ autogenerate: { directory: 'calculo' } }],
 				},
 				{
   					label: 'RESULTADOS',
+					collapsed: true, // <--- Hace que esta carpeta aparezca replegada
   					items: [
     					{
       						label: 'Gráficas',
       						items: [
-        						{ autogenerate: { directory: 'resultados' } },
+        						{ autogenerate: { directory: 'resultados/graficas' } },
       						],
     					},
 						    					{
       						label: 'Listados',
       						items: [
-        						{ autogenerate: { directory: 'resultados' } },
+        						{ autogenerate: { directory: 'resultados/listados' } },
       						],
     					},
     					{
       						label: 'Planos',
       						items: [
-        						{ autogenerate: { directory: 'resultados' } },
+        						{ autogenerate: { directory: 'resultados/planos' } },
       						],
    						 },
  					 ],
